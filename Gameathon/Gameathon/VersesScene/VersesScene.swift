c6b04fc75705e59333503974f40abcb1a68d3d1a
Samar Younan <eng.samaryounan@gmail.com>
2020-12-25 14:58:29 +0200

    add design assets for verses screen

--- Gameathon/Gameathon/VersesScene/VersesScene.swift
+++ Gameathon/Gameathon/VersesScene/VersesScene.swift
@@ -38,7 +38,7 @@ class VersesScene: SKScene {
         scoreLabel.fontColor = .red
         scoreLabel.horizontalAlignmentMode = .right
         scoreLabel.position = CGPoint(x: self.frame.midX + 110, y: 150)
-        addChild(scoreLabel)
+//        addChild(scoreLabel)
   
         // game over
         self.backgroundColor = SKColor.black
@@ -126,6 +126,12 @@ class VersesScene: SKScene {
         } else if (node.name == "backBtn") {
             guard let levelsScene = LevelsScene(fileNamed: "LevelsScene") else { return }
             self.view?.presentScene(levelsScene, transition: SKTransition.moveIn(with: .left, duration: 0.5))
+            
+        } else if (node.name == "homeBtn") {
+            
+            // navigate to selection track screen
+            guard let selectionScene = SelectionScene(fileNamed: "SelectionScene") else { return }
+            self.view?.presentScene(selectionScene, transition: SKTransition.moveIn(with: .right, duration: 0.5))
         }
         
         UserDefaults.standard.set(score, forKey: "score")
@@ -150,7 +156,7 @@ class GameOverScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
 
-        self.backgroundColor = SKColor.darkGray
+        self.backgroundColor = SKColor.init(hex: "#003F65") ?? SKColor.darkGray
 
         addChild(notificationLabel)
         notificationLabel.fontSize = 32.0
@@ -175,5 +181,34 @@ class GameOverScene: SKScene {
 
         let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
         view?.presentScene(gameScene, transition: reveal)
+    }
+}
+
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 7 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
     }
 }
